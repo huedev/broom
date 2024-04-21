b3201872915068a74fb7927a42539a6fe55242ed
hue <32443158+huedev@users.noreply.github.com>
2024-04-20 21:40:01 -0400

    More info in debug screen

--- src/main/java/me/huedev/broom/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/me/huedev/broom/mixin/client/gui/hud/InGameHudMixin.java
@@ -1,31 +1,39 @@
 package me.huedev.broom.mixin.client.gui.hud;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.class_564;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.hit.HitResultType;
+import net.minecraft.world.LightType;
+import net.minecraft.world.chunk.Chunk;
+import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.util.math.Direction;
+import net.modificationstation.stationapi.api.worldgen.BiomeAPI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin extends DrawContext {
     @Shadow
     private Minecraft minecraft;
 
-    // X Position
-    @ModifyConstant(method = "render", constant = @Constant(stringValue = "x: "))
-    private String broom_renameXPosition(String value) {
-        return "X: ";
-    }
-
-    // Y Position
-    @ModifyConstant(method = "render", constant = @Constant(stringValue = "y: "))
-    private String broom_renameYPosition(String value) {
-        return "Y: ";
+    @ModifyArg(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
+                    ordinal = 2
+            ),
+            index = 1
+    )
+    private String broom_removeVanillaXPosition(String value) {
+        return "";
     }
 
     @ModifyArg(
@@ -35,16 +43,10 @@ public class InGameHudMixin extends DrawContext {
                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 3
             ),
-            index = 3
+            index = 1
     )
-    private int broom_alignYPosition(int value) {
-        return 74;
-    }
-
-    // Z Position
-    @ModifyConstant(method = "render", constant = @Constant(stringValue = "z: "))
-    private String broom_renameZPosition(String value) {
-        return "Z: ";
+    private String broom_removeVanillaYPosition(String value) {
+        return "";
     }
 
     @ModifyArg(
@@ -54,13 +56,12 @@ public class InGameHudMixin extends DrawContext {
                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 4
             ),
-            index = 3
+            index = 1
     )
-    private int broom_alignZPosition(int value) {
-        return 84;
+    private String broom_removeVanillaZPosition(String value) {
+        return "";
     }
 
-    // Facing
     @ModifyArg(
             method = "render",
             at = @At(
@@ -70,22 +71,8 @@ public class InGameHudMixin extends DrawContext {
             ),
             index = 1
     )
-    private String broom_facingDirection(String value) {
-        Direction facing = Direction.fromRotation(this.minecraft.player.yaw);
-        return "Facing: " + facing.toString();
-    }
-
-    @ModifyArg(
-            method = "render",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
-                    ordinal = 5
-            ),
-            index = 3
-    )
-    private int broom_alignFacing(int value) {
-        return 94;
+    private String broom_removeVanillaFacing(String value) {
+        return "";
     }
 
     // Seed
@@ -95,9 +82,50 @@ public class InGameHudMixin extends DrawContext {
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 5
-            )
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT
     )
-    private void broom_additionalDebugInfo(CallbackInfo ci, @Local TextRenderer var8) {
-        this.drawTextWithShadow(var8, "Seed: " + minecraft.world.getSeed(), 2, 104, 14737632);
+    private void broom_additionalDebugInfo(float bl, boolean i, int j, int par4, CallbackInfo ci, class_564 scaler, int var6, int var7, TextRenderer var8) {
+        int offset = 52;
+
+        double playerX = minecraft.player.x;
+        double playerY = minecraft.player.y - 1;
+        double playerZ = minecraft.player.z;
+        String text = "XYZ: " + String.format("%.4f", playerX) + " " + String.format("%.4f", playerY) + " " + String.format("%.4f", playerZ);
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        double playerYaw = minecraft.player.yaw;
+        double playerPitch = minecraft.player.pitch;
+        Direction facing = Direction.fromRotation(playerYaw);
+        text = "Facing: " + facing + " (" + String.format("%.1f", Math.abs(playerYaw % 360.0F)) + " " + String.format("%.1f", playerPitch % 360.0F) + ")";
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        int blockX = (int) Math.floor(minecraft.player.x);
+        int blockY = (int) Math.floor(minecraft.player.y) - 1;
+        int blockZ = (int) Math.floor(minecraft.player.z);
+        int blockRelativeX = blockX & 15;
+        int blockRelativeY = blockY & 15;
+        int blockRelativeZ = blockZ & 15;
+        text = "Block: " + blockX + " " + blockY + " " + blockZ + " [" + blockRelativeX + " " + blockRelativeY + " " + blockRelativeZ + "]";
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        int chunkX = blockX >> 4;
+        int chunkY = blockY >> 4;
+        int chunkZ = blockZ >> 4;
+        text = "Chunk: " + chunkX + " " + chunkY + " " + chunkZ;
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        text = "Biome: " + minecraft.world.method_1781().method_1787(blockX, blockZ).name;
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        Chunk chunk = minecraft.world.method_199(blockX, blockZ);
+        int skyLight = chunk.getLight(LightType.SKY, blockRelativeX, blockY, blockRelativeZ);
+        int blockLight = chunk.getLight(LightType.BLOCK, blockRelativeX, blockY, blockRelativeZ);
+        int light = Math.max(skyLight, blockLight);
+        text = "Light: " + light + " (" + skyLight + " sky, " + blockLight + " block)";
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
+
+        text = "Seed: " + minecraft.world.getSeed();
+        this.drawTextWithShadow(var8, text, 2, offset += 10, 14737632);
     }
 }
