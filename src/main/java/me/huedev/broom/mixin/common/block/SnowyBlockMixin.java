a9d362575ececf0b9473b6f228bfe86f36635036
hue <32443158+huedev@users.noreply.github.com>
2024-04-23 01:01:31 -0400

    Snow layers
    Tweaked snow crafting recipe

--- src/main/java/me/huedev/broom/mixin/common/block/SnowyBlockMixin.java
+++ src/main/java/me/huedev/broom/mixin/common/block/SnowyBlockMixin.java
@@ -5,16 +5,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.SnowyBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
+import net.modificationstation.stationapi.api.block.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Random;
+import java.util.Collections;
+import java.util.List;
 
 @Mixin(SnowyBlock.class)
 public class SnowyBlockMixin extends Block {
@@ -26,6 +31,50 @@ public class SnowyBlockMixin extends Block {
 
     public SnowyBlockMixin(int id, int textureId, Material material) {
         super(id, textureId, material);
+    }
+
+    @Inject(method = "getCollisionShape", at = @At("RETURN"), cancellable = true)
+    public void broom_getCollisionShape(World world, int x, int y, int z, CallbackInfoReturnable<Box> cir) {
+        int layers = world.getBlockMeta(x, y, z);
+        if (layers > 3) {
+            Box box = Box.create((double) x + 0.0F, (double) y + 0.0F, (double) z + 0.0F, (double) x + 1.0F, (double) y + 0.5F, (double) z + 1.0F);
+            cir.setReturnValue(box);
+        } else {
+            cir.setReturnValue(null);
+        }
+
+        /*
+        int layers = world.getBlockMeta(x, y, z);
+        float height = (float)(2 * layers) / 16.0F;
+        Box box = Box.create((double) x + 0.0F, (double) y + 0.0F, (double) z + 0.0F, (double) x + 1.0F, (double) y + height, (double) z + 1.0F);
+        cir.setReturnValue(box);
+        */
+    }
+
+    @Override
+    public boolean onUse(World world, int x, int y, int z, PlayerEntity player) {
+        ItemStack stack = player.getHand();
+        if (stack == null) return false;
+
+        Item item = stack.getItem();
+        if (!(item instanceof BlockItem blockItem)) return false;
+
+        if (blockItem.getBlock() != this) return false;
+
+        int blockId = world.getBlockId(x, y, z);
+        if (blockId != this.id) return false;
+
+        int meta = world.getBlockMeta(x, y, z);
+
+        if (meta < 7) {
+            meta += 1;
+            world.method_215(x, y, z, meta);
+            world.playSound(x + 0.5, y + 0.5, z + 0.5, this.soundGroup.getSound(), 1.0F, 1.0F);
+            world.method_246(x, y, z);
+
+            stack.count--;
+        }
+        return true;
     }
 
     @Override
@@ -42,6 +91,7 @@ public class SnowyBlockMixin extends Block {
         this.dropStacks(world, x, y, z, meta);
     }
 
+    /*
     @Inject(at = @At("RETURN"), method = "getDroppedItemCount", cancellable = true)
     public void broom_getDroppedItemCount(Random random, CallbackInfoReturnable<Integer> cir) {
         if (brokenBySilkTouchTool || brokenByNonSilkTouchShovel) {
@@ -60,5 +110,19 @@ public class SnowyBlockMixin extends Block {
             brokenBySilkTouchTool = false;
             brokenByNonSilkTouchShovel = false;
         }
+    }
+    */
+
+    @Override
+    public List<ItemStack> getDropList(World world, int x, int y, int z, BlockState state, int meta) {
+        if (brokenBySilkTouchTool) {
+            brokenBySilkTouchTool = false;
+            brokenByNonSilkTouchShovel = false;
+            return Collections.singletonList(new ItemStack(this.asItem(), meta + 1, 0));
+        } else if (brokenByNonSilkTouchShovel) {
+            brokenByNonSilkTouchShovel = false;
+            return Collections.singletonList(new ItemStack(Item.SNOWBALL, meta + 1, 0));
+        }
+        return Collections.emptyList();
     }
 }
