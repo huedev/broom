a301be324843ae694eefc674cda9cdcffa4fb627
hue <32443158+huedev@users.noreply.github.com>
2024-04-30 01:05:27 -0400

    Wooden button

--- src/main/java/me/huedev/broom/block/BroomBlocks.java
+++ src/main/java/me/huedev/broom/block/BroomBlocks.java
@@ -39,6 +39,7 @@ public class BroomBlocks {
     public static Block STONE_BRICK_SLAB;
     public static Block STONE_BRICK_DOUBLE_SLAB;
     public static Block STONE_BRICK_STAIRS;
+    public static Block WOODEN_BUTTON;
     public static Block IRON_TRAPDOOR;
 
     public static void init() {
@@ -81,6 +82,8 @@ public class BroomBlocks {
         STONE_BRICK_SLAB = new BroomSlabBlock(Broom.id("stone_brick_slab"), BroomBlocks.STONE_BRICKS);
         STONE_BRICK_DOUBLE_SLAB = new BroomDoubleSlabBlock(Broom.id("stone_brick_double_slab"), BroomBlocks.STONE_BRICKS);
         STONE_BRICK_STAIRS = new TemplateStairsBlock(Broom.id("stone_brick_stairs"), BroomBlocks.STONE_BRICKS).setTranslationKey(Broom.id("stone_brick_stairs"));
+
+        WOODEN_BUTTON = new BroomButtonBlock(Broom.id("wooden_button"), Block.PLANKS, 30);
 
         IRON_TRAPDOOR = new TemplateTrapdoorBlock(Broom.id("iron_trapdoor"), Material.METAL).setTranslationKey(Broom.id("iron_trapdoor")).setHardness(5.0F).setSoundGroup(Block.METAL_SOUND_GROUP).disableTrackingStatistics().ignoreMetaUpdates();
 
