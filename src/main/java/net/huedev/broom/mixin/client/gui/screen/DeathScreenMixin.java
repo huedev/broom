cbacc13c535a0fa3865dba98834cda8ee57a2d66
hue <32443158+huedev@users.noreply.github.com>
2024-10-20 01:23:35 -0400

    Fix for death screen formatting
    Thanks DanyGames2014!

---
+++ src/main/java/net/huedev/broom/mixin/client/gui/screen/DeathScreenMixin.java
@@ -0,0 +1,18 @@
+package net.huedev.broom.mixin.client.gui.screen;
+
+import net.minecraft.client.gui.screen.DeathScreen;
+import net.modificationstation.stationapi.api.util.Formatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+/**
+ * @author DanyGames2014
+ */
+@Mixin(DeathScreen.class)
+public class DeathScreenMixin {
+    @ModifyConstant(method = "render", constant = @Constant(stringValue = "Score: &e"), require = 0)
+    public String fixFormattingCharacter(String constant){
+        return constant.replace('&', Formatting.FORMATTING_CODE_PREFIX);
+    }
+}
